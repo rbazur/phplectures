e195567657faccec00f4af7fe68009d797d04571
rbazur <razur@udm.edu.ph>
2024-09-16 23:03:43 -0700

    New Lectures

---
+++ newlectures/14filehandling.php
@@ -0,0 +1,28 @@
+<?php
+echo '<h1>File Handling</h1>';
+/* ---------- File Handling --------- */
+
+/* 
+  File handling is the ability to read and write files on the server.
+  PHP has built in functions for reading and writing files.
+*/
+
+$file = 'extras/users.txt';
+
+if (file_exists($file)) {
+  // echo readfile($file); //Read the content | number of bytes 
+  //fopen() - gives more control over the file
+  $handle = fopen($file, 'r');
+
+  $contents = fread($handle, filesize($file));
+  fclose($handle);
+   echo nl2br($contents);
+} else {
+  // echo 'File does not exist';
+  //Create the file
+  $handle = fopen($file, 'w');
+  $contents = 'Ryan' . PHP_EOL . 'Giselle' . PHP_EOL . 'Mikaela' . PHP_EOL . 'Eugene';
+  fwrite($handle, $contents);
+  fclose($handle);
+}
+?>
